365f7387fe85b5a63a02a73631f481895738cc66
EmperorRAG <mrbrewermj@gmail.com>
2026-03-24 19:11:37 +0200

    feature: Add get-coffees tool with coffee data

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,58 @@ const server = new McpServer({
 	version: "1.0.0",
 });
 
+export const coffeeDrinks = [
+	{
+		id: 1,
+		name: "Flat White",
+		size: "Medium",
+		price: 4.5,
+		iced: false,
+		caffeineMg: 130,
+	},
+	{
+		id: 2,
+		name: "Cappuccino",
+		size: "Small",
+		price: 3.75,
+		iced: false,
+		caffeineMg: 80,
+	},
+	{
+		id: 3,
+		name: "Latte",
+		size: "Large",
+		price: 5.25,
+		iced: true,
+		caffeineMg: 150,
+	},
+	{
+		id: 4,
+		name: "Espresso",
+		size: "Small",
+		price: 2.5,
+		iced: false,
+		caffeineMg: 64,
+	},
+];
+
+server.registerTool(
+	"get-coffees",
+	{
+		description: "Get a list of all coffees",
+	},
+	async () => {
+		return {
+			content: [
+				{
+					type: "text",
+					text: JSON.stringify(coffeeDrinks),
+				},
+			],
+		};
+	},
+);
+
 async function main() {
 	const transport = new StdioServerTransport();
 	await server.connect(transport);
